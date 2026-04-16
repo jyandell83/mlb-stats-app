c30ee5649d7a9e0584ce2c218a8fb5d8112ce608
Jeremy Yandell <jereyand@gmail.com>
2026-04-16 11:42:05 -0700

    add highlight functionality, still working on this and also adds record

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [games, setGames] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [highlightId, setHighlightId] = useState(null);
 
   const today = new Date();
   const formattedDate = today.toISOString().split("T")[0];
@@ -16,6 +17,14 @@ export default function App() {
         .then((data) => {
           const allGames = data.dates?.[0]?.games || [];
           setGames(allGames);
+          //just setting first game to highlight, eventually intention is to flash when score changes
+          if (allGames.length > 0) {
+            setHighlightId(allGames[0].gamePk);
+
+            setTimeout(() => {
+              setHighlightId(null);
+            }, 1000);
+          }
           setLoading(false);
         })
         .catch((err) => {
@@ -24,29 +33,14 @@ export default function App() {
         });
     };
 
-    // ✅ run immediately
+    // run immediately
     fetchGames();
 
-    // ✅ then run every 30 sec
+    // then run every 30 sec
     const interval = setInterval(fetchGames, 30000);
 
     return () => clearInterval(interval);
   }, [formattedDate]);
-
-  // useEffect(() => {
-
-  //   fetch(`https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${formattedDate}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const allGames = data.dates?.[0]?.games || [];
-  //       setGames(allGames);
-  //       setLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //       setLoading(false);
-  //     });
-  // }, [formattedDate]);
 
   if (loading) {
     return <div className="loading">Loading games...</div>;
@@ -71,13 +65,22 @@ export default function App() {
           const away = game.teams.away.team.name;
           const homeScore = game.teams.home.score ?? "-";
           const awayScore = game.teams.away.score ?? "-";
+          const homeTeamWins = game.teams.home.leagueRecord.wins;
+          const homeTeamLosses = game.teams.home.leagueRecord.losses;
+          const awayTeamWins = game.teams.away.leagueRecord.wins;
+          const awayTeamLosses = game.teams.away.leagueRecord.losses;
           const status = game.status.detailedState;
           const gameTime = new Date(game.gameDate).toLocaleTimeString();
 
           return (
-            <li key={game.gamePk} className="gameCard">
+            <li
+              className={`gameCard ${
+                highlightId === game.gamePk ? "highlight" : ""
+              }`}
+            >
               <div className="font-semibold">
-                {away} @ {home}
+                {away} Record: {awayTeamWins} - {awayTeamLosses} @ {home}
+                Record: {homeTeamWins} - {homeTeamLosses}
               </div>
               <div className="score">
                 Score: {awayScore} - {homeScore}
