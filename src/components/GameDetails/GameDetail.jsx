5212afc36009b3be7c940bda654f62bdf5559d9a
Jeremy Yandell <jereyand@gmail.com>
2026-04-20 21:52:42 -0700

    adding fields to game details

--- src/components/GameDetails/GameDetail.jsx
+++ src/components/GameDetails/GameDetail.jsx
@@ -5,13 +5,24 @@ export default function GameDetail({ selectedGamePk, gameDetails }) {
         <div className="details">
           <div>
             {selectedGamePk}
+            {gameDetails.liveData.linescore.outs}
             {gameDetails.liveData.linescore.inningState}{" "}
             {gameDetails.liveData.linescore.currentInning}
           </div>
 
-          <div>
-            {gameDetails.liveData.plays.currentPlay?.result?.description ??
-              "No play yet"}
+          <div className="flex flex-col">
+            <div>
+              {gameDetails.liveData.plays.currentPlay?.result?.description ??
+                "No play yet"}
+            </div>
+            <div>{`${gameDetails.liveData.linescore.outs} Out`}</div>
+            <div>
+              Count:{" "}
+              {`${gameDetails.liveData.linescore.balls} - ${gameDetails.liveData.linescore.strikes}`}
+            </div>
+
+            <div>{`P ${gameDetails.liveData.linescore.defense.pitcher.fullName}`}</div>
+            <div>{`${gameDetails.liveData.linescore.offense.batter.fullName} at Bat`}</div>
           </div>
         </div>
       )}
