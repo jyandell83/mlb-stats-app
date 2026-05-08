d29788809b00109ebd14f97e65a7df4e47ba6458
Jeremy Yandell <jereyand@gmail.com>
2026-05-08 10:07:20 -0700

    showing rough box score with stats in recap

--- src/components/BoxScore/BoxScore.jsx
+++ src/components/BoxScore/BoxScore.jsx
@@ -30,18 +30,66 @@ const BoxScore = ({ gamePk }) => {
 
   return (
     <div className="flex justify-evenly">
-      <div>
+      <div className="card">
         <div>{boxScore?.teams?.away.team.name}</div>
-        {awayLineup.map((player) => (
-          <div key={player.person.id}>{player.person.fullName}</div>
-        ))}
+        <table className="stats-table">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>AB</th>
+              <th>R</th>
+              <th>H</th>
+              <th>RBI</th>
+              <th>BB</th>
+              <th>SO</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {awayLineup.map((player) => (
+              <tr key={player.person.id}>
+                <td>{`${player.person.boxscoreName} (${player.position.abbreviation})`}</td>
+                <td>{player.stats?.batting?.atBats ?? "-"}</td>
+                <td>{player.stats?.batting?.runs ?? "-"}</td>
+                <td>{player.stats?.batting?.hits ?? "-"}</td>
+                <td>{player.stats?.batting?.rbi ?? "-"}</td>
+                <td>{player.stats?.batting?.baseOnBalls ?? "-"}</td>
+                <td>{player.stats?.batting?.strikeOuts ?? "-"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
-      <div>
+      <div className="card">
         <div>{boxScore?.teams?.home.team.name}</div>
-        {homeLineup.map((player) => (
-          <div key={player.person.id}>{player.person.fullName}</div>
-        ))}
+        <table className="stats-table">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>AB</th>
+              <th>R</th>
+              <th>H</th>
+              <th>RBI</th>
+              <th>BB</th>
+              <th>SO</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {homeLineup.map((player) => (
+              <tr key={player.person.id}>
+                <td>{`${player.person.boxscoreName} (${player.position.abbreviation})`}</td>
+                <td>{player.stats?.batting?.atBats ?? "-"}</td>
+                <td>{player.stats?.batting?.runs ?? "-"}</td>
+                <td>{player.stats?.batting?.hits ?? "-"}</td>
+                <td>{player.stats?.batting?.rbi ?? "-"}</td>
+                <td>{player.stats?.batting?.baseOnBalls ?? "-"}</td>
+                <td>{player.stats?.batting?.strikeOuts ?? "-"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
