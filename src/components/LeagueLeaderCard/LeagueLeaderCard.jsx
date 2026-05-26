a360dc917e797b0d46742acb67306472c6f88275
Jeremy Yandell <jereyand@gmail.com>
2026-05-26 11:30:19 -0700

    add league leaders functionality, adds cards with 6 categories to leaderboards with season stats

--- src/components/LeagueLeaderCard/LeagueLeaderCard.jsx
+++ src/components/LeagueLeaderCard/LeagueLeaderCard.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+
+import { getLeagueLeaders } from "../../api/mlbApi";
+
+import StatsTable from "../StatsTable/StatsTable";
+
 const LeagueLeaderCard = ({ title, category, statGroup }) => {
-  return <div>{`${title},${category},${statGroup}`}</div>;
+  const [leaders, setLeaders] = useState([]);
+  useEffect(() => {
+    const fetchLeaders = async () => {
+      try {
+        const res = await fetch(
+          getLeagueLeaders({
+            category,
+            statGroup,
+          }),
+        );
+
+        const data = await res.json();
+
+        const formattedLeaders =
+          data?.leagueLeaders?.[0]?.leaders?.map((leader, index) => ({
+            rank: index + 1,
+            player: leader?.person?.fullName,
+            team: leader.team?.name,
+            value: leader?.value,
+          })) ?? [];
+
+        setLeaders(formattedLeaders);
+      } catch (err) {
+        console.error("Failed to fetch league leaders:", err);
+      }
+    };
+
+    fetchLeaders();
+  }, [category, statGroup]);
+
+  const columns = [
+    { label: "#", key: "rank" },
+    { label: "Player", key: "player" },
+    { label: "Team", key: "team" },
+    { label: title, key: "value" },
+  ];
+  return (
+    <section className="leader-card card">
+      <h3>{title}</h3>
+
+      <StatsTable columns={columns} rows={leaders} />
+    </section>
+  );
 };
 
 export default LeagueLeaderCard;
